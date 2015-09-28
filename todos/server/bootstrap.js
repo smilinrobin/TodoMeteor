2cdcd0b663ab124c1352461cb25585e9d773cb25
Robin Gupta <robin.gupta@pearson.com>
2015-09-28 10:16:01 +0530

    first commit

---
+++ todos/server/bootstrap.js
@@ -0,0 +1,29 @@
+// if the database is empty on server start, create some sample data.
+Meteor.startup(function () {
+  if (Lists.find().count() === 0) {
+    var data = [
+      {name: "GGMB DEV Todos",
+       items: ["Backlinking",
+         "Bundle creation",
+         "Daalt seeding support",
+         "Revel Mobile support",
+         "Full Stack Automation",
+         "Some Documentation"
+       ]
+      }
+    ];
+
+    var timestamp = (new Date()).getTime();
+    _.each(data, function(list) {
+      var list_id = Lists.insert({name: list.name,
+        incompleteCount: list.items.length});
+
+      _.each(list.items, function(text) {
+        Todos.insert({listId: list_id,
+                      text: text,
+                      createdAt: new Date(timestamp)});
+        timestamp += 1; // ensure unique timestamp.
+      });
+    });
+  }
+});
